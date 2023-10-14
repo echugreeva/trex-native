2dc2dca0322efa60ac689ce4a19c07c47049d039
echugreeva <e.chugreeva@gmail.com>
2023-10-14 19:26:38 +0300

    exandable on card

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ImageBackground, Image } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, SafeAreaView, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons'; 
-import { Ionicons } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
-const trips =  [
+
+const trips = [
   {
     "id": "1",
     "trip_title": "Tropical Paradise Getaway",
     "location": "Maldives",
     "date": "July 15, 2024 - July 22, 2024",
     "description": "Escape to the breathtaking beaches and crystal-clear waters of the Maldives. Enjoy a week of relaxation, water sports, and island exploration in luxurious overwater bungalows.",
-    "images": ["https://images.pexels.com/photos/1287460/pexels-photo-1287460.jpeg?auto=compress&cs=tinysrgb&w=800", 
-    "https://images.pexels.com/photos/1320684/pexels-photo-1320684.jpeg?auto=compress&cs=tinysrgb&w=800"],
-    "participants":[],
+    "images": ["https://images.pexels.com/photos/1287460/pexels-photo-1287460.jpeg?auto=compress&cs=tinysrgb&w=800",
+      "https://images.pexels.com/photos/1320684/pexels-photo-1320684.jpeg?auto=compress&cs=tinysrgb&w=800"],
+    "participants": [],
     "messages": [{
-      "time":"",
+      "time": "",
       "userId": "",
-      "userName":"admin",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
     }]
@@ -32,35 +33,35 @@ const trips =  [
     "location": "Swiss Alps",
     "date": "January 10, 2025 - January 18, 2025",
     "description": "Embrace the winter wonderland of the Swiss Alps with this thrilling adventure trip. Ski and snowboard down powdery slopes, indulge in Swiss cuisine, and take in the stunning mountain views.",
-"images":[ "https://images.pexels.com/photos/15964998/pexels-photo-15964998/free-photo-of-cold-snow-landscape-mountains.jpeg?auto=compress&cs=tinysrgb&w=800"],
-"participants":[],
-"messages": [{
-  "time":"",
-  "userId": "",
-  "userName":"admin",
+    "images": ["https://images.pexels.com/photos/15964998/pexels-photo-15964998/free-photo-of-cold-snow-landscape-mountains.jpeg?auto=compress&cs=tinysrgb&w=800"],
+    "participants": [],
+    "messages": [{
+      "time": "",
+      "userId": "",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
-}]
+    }]
   },
   {
     "id": "3",
     "trip_title": "Historic European Capitals Tour",
     "location": "London, Paris, Rome",
     "date_start": "April 5, 2024",
-    "date_end":"  April 20, 2024",
+    "date_end": "  April 20, 2024",
     "description": "Embark on a cultural journey through three iconic European capitals. Explore the historical landmarks, museums, and indulge in the local delicacies of London, Paris, and Rome.",
-    "image": ["https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&w=800", 
-    "https://images.pexels.com/photos/1427581/pexels-photo-1427581.jpeg?auto=compress&cs=tinysrgb&w=800",
-"https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg?auto=compress&cs=tinysrgb&w=800"
-],
-"participants":[],
-"messages": [{
-  "time":"",
-  "userId": "",
-  "userName":"admin",
+    "image": ["https://images.pexels.com/photos/1530259/pexels-photo-1530259.jpeg?auto=compress&cs=tinysrgb&w=800",
+      "https://images.pexels.com/photos/1427581/pexels-photo-1427581.jpeg?auto=compress&cs=tinysrgb&w=800",
+      "https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg?auto=compress&cs=tinysrgb&w=800"
+    ],
+    "participants": [],
+    "messages": [{
+      "time": "",
+      "userId": "",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
-}]
+    }]
   },
   {
     "id": "4",
@@ -69,11 +70,11 @@ const trips =  [
     "date": "September 3, 2024 - September 10, 2024",
     "description": "Witness the incredible wildlife of the Serengeti on an unforgettable safari adventure. Spot lions, elephants, giraffes, and more as you traverse the vast plains of Tanzania.",
     "images": ["https://images.pexels.com/photos/259547/pexels-photo-259547.jpeg?auto=compress&cs=tinysrgb&w=800"],
-    "participants":[],
+    "participants": [],
     "messages": [{
-      "time":"",
+      "time": "",
       "userId": "",
-      "userName":"admin",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
     }]
@@ -85,11 +86,11 @@ const trips =  [
     "date": "November 12, 2024 - November 24, 2024",
     "description": "Satisfy your taste buds on this culinary journey through Tokyo, Bangkok, and Singapore. Experience a diverse range of Asian cuisines and street food delights.",
     "images": ["https://images.pexels.com/photos/1031780/pexels-photo-1031780.jpeg?auto=compress&cs=tinysrgb&w=800"],
-    "participants":[],
+    "participants": [],
     "messages": [{
-      "time":"",
+      "time": "",
       "userId": "",
-      "userName":"admin",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
     }]
@@ -101,11 +102,11 @@ const trips =  [
     "date": "May 1, 2025 - May 10, 2025",
     "description": "Immerse yourself in the unparalleled biodiversity of the Amazon Rainforest. Hike through lush jungles, spot exotic wildlife, and learn about indigenous cultures.",
     "images": ["https://images.pexels.com/photos/325807/pexels-photo-325807.jpeg?auto=compress&cs=tinysrgb&w=800"],
-    "participants":[],
+    "participants": [],
     "messages": [{
-      "time":"",
+      "time": "",
       "userId": "",
-      "userName":"admin",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
     }]
@@ -117,11 +118,11 @@ const trips =  [
     "date": "August 20, 2024 - August 30, 2024",
     "description": "Hit the open road along California's scenic coast. Explore vibrant cities, visit famous landmarks, and taste exquisite wines in Napa Valley.",
     "images": ["https://images.pexels.com/photos/3605316/pexels-photo-3605316.jpeg?auto=compress&cs=tinysrgb&w=800"],
-    "participants":[],
+    "participants": [],
     "messages": [{
-      "time":"",
+      "time": "",
       "userId": "",
-      "userName":"admin",
+      "userName": "admin",
       "message": "welcome to your trip chat"
 
     }]
@@ -134,38 +135,48 @@ const trips =  [
 export default function App() {
   const [tripToShow, setTrip] = useState(trips[0])
   const [imgIndex, setIndex] = useState(0)
+  const [imgUrl, setUrl] = useState(tripToShow.images[imgIndex])
+  const [expanded, setExpanded] = useState(false)
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  useEffect(() => {
+
+  }, [imgIndex])
 
   const scrollImg = (direction) => {
-    let length = tripToShow[0].image.length
+    let length = tripToShow.images.length
 
     console.log(length)
     console.log(imgIndex)
     if (direction == 'forward' && imgIndex < length - 1) {
 
-        setIndex(imgIndex + 1)
+      setIndex(imgIndex + 1)
 
     }
     if (direction == 'back' && imgIndex >= 1) {
 
-        setIndex(imgIndex - 1)
+      setIndex(imgIndex - 1)
 
     }
     if (direction == 'back' && imgIndex === 0) {
 
-        setIndex(length - 1)
+      setIndex(length - 1)
 
 
     }
     if (direction == 'forward' && imgIndex === length - 1) {
 
-        setIndex(0)
+      setIndex(0)
 
 
     }
 
     console.log(imgIndex)
 
-}
+  }
   return (
     // <NavigationContainer>
     //   <View>
@@ -180,21 +191,34 @@ export default function App() {
     //   </View>
 
     // </NavigationContainer>
-    <SafeAreaView style={[styles.container, { backgroundColor: 'teal' }]}>
+    <SafeAreaView style={[styles.container, { backgroundColor: 'white' }]}>
       <View style={[styles.container, { justifyContent: 'space-between', width: '100%' }]}>
-        <View style={{height:"85%", width:'100%'}}>
+        <View style={{ height: "90%", width: '100%' }}>
           {/* <Image
            source={{uri:'https://images.pexels.com/photos/1320684/pexels-photo-1320684.jpeg?auto=compress&cs=tinysrgb&w=800'}}
            style={{height: '100%', width: '100%'}}
           /> */}
-          <ImageBackground  
-          source={{uri:'https://images.pexels.com/photos/1287460/pexels-photo-1287460.jpeg?auto=compress&cs=tinysrgb&w=800'}}
-          style={{height: '100%', width: '100%', resizeMode: 'contain'}}>
+          <ImageBackground
+            source={{ uri: imgUrl }}
+            style={{ height: '100%', width: '100%', resizeMode: 'contain' }}>
             <Text>{tripToShow.trip_title}</Text>
             <Text>{tripToShow.location}</Text>
-            
+            <View style={styles.expandable}>
+              <TouchableOpacity onPress={ toggleExpanded }>
+                <AntDesign name="infocirlceo" size={24} color="black" />
+              </TouchableOpacity>
+              {expanded && <View>
+                <Text>Trip details:</Text>
+                <Text>
+                {tripToShow.description}
+                </Text>
+                </View>}
+
+            </View>
+
+
           </ImageBackground>
-          
+
         </View>
         <View style={styles.bottom}>
           <TouchableOpacity><AntDesign name="close" size={40} color="red" />
@@ -229,5 +253,5 @@ const styles = StyleSheet.create({
 
 
   },
-  
+
 });
